56055ced380dc6e30361bb4c627b02904a56c003
pythagoras58 <solomondanso58@gmail.com>
2023-01-24 23:45:40 +0000

    Add project files.

---
+++ recipeAPI/Controllers/ReceipeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace recipeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReceipeController : ControllerBase
+    {
+        [HttpGet]
+        public string[] getDishes()
+        {
+            string[] dishes = {"Fufu", "Banku", "Yam"};
+            return dishes;
+        }
+    }
+}
